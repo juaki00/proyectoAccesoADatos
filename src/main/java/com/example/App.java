060faef6a6ff9d6d6952daf5858a655c056ad580
juaki <juaki00@gmail.com>
2023-11-26 17:28:05 +0100

    Añado loadXML en la clase App

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -11,14 +11,20 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.IOException;
 
 public class App extends Application {
+    @Getter
+    @Setter
+    private static Stage myStage;
     @Override
     public void start(Stage stage) throws IOException {
+        myStage = stage;
         FXMLLoader fxmlLoader = new FXMLLoader( App.class.getResource( "demo/login-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 341, 398);
+        Scene scene = new Scene(fxmlLoader.load(), 900, 800);
         stage.setTitle("Login");
         stage.setScene(scene);
         stage.show();
@@ -26,16 +32,18 @@ public class App extends Application {
 
     public static void main(String[] args) {
         launch();
+    }
 
-//        StudentDAO studentDAO = new StudentDAO();
-//        TeacherDAO teacherDAO = new TeacherDAO();
-//        CompanyDAO companyDAO = new CompanyDAO();
-//        ActivityDAO activityDAO = new ActivityDAO();
-//        System.out.println( studentDAO.getAll() );
-//        System.out.println( teacherDAO.getAll() );
-//        System.out.println( companyDAO.getAll() );
-//
-//        Student joaquin = studentDAO.getAll().get( 0 );
-//        System.out.println( activityDAO.activitiesOfStudent( joaquin ) );
+    public static void loadFXML(String fxml, String titulo){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("demo/"+fxml));
+            Scene scene = new Scene(fxmlLoader.load());
+            myStage.setTitle(titulo);
+            myStage.setScene(scene);
+            myStage.show();
+        } catch (IOException e) {
+            System.out.println("Error al cargar el archivo "+fxml);
+            throw new RuntimeException(e);
+        }
     }
 }
