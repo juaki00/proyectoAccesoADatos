ef3146dedcb17ad2d36e28c4e37082cc129c2aa5
Francisco-Diaz-Roldan <pacodiaz008@gmail.com>
2023-12-10 12:45:44 +0100

    Se ha borrado la clase EditarAlumno

--- src/main/java/com/example/demo/EditarAlumno.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo;
-
-public class EditarAlumno
-{
-    @javafx.fxml.FXML
-    public void initialize() {
-    }}
--- src/main/java/com/example/diaryActivity/DiaryActivity.java
+++ src/main/java/com/example/diaryActivity/DiaryActivity.java
@@ -5,7 +5,6 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.experimental.Accessors;
 
 import java.time.LocalDate;
 import java.util.Date;
